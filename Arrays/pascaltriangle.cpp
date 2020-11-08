b52620dce542a0ae1965cec0af615051c93621d2
pranathnaik <pranathnaik12@gmail.com>
2020-11-08 23:01:24 +0530

    dead inside

--- Arrays/pascaltriangle.cpp
+++ Arrays/pascaltriangle.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 int main()
 { 
-    
     vector <int> vec;
     cout << vec.size();
     int n = 5;
@@ -16,12 +15,10 @@ int main()
     int small = vec[0];
     for (int i = 0; i < n; i++)
     {
-        if (small > vec[i])
-            small = vec[i];
+        if (small > vec[i]) small = vec[i];
     }
     cout << endl
          << small;
     vec.push_back(small);
-
         cout << vec.size();
 }
