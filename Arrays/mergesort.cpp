a5353cacc65271149d3990e08748930cffd94644
pranathnaik <pranathnaik12@gmail.com>
2020-11-11 22:56:11 +0530

    incomplete

--- Arrays/mergesort.cpp
+++ Arrays/mergesort.cpp
@@ -7,4 +7,11 @@ using namespace std;
 
      for(int i = 0; i < size ; i++){
          cin>>a[i];
-      }
+
+        int tempSize=size;
+           tempSize=tempSize/2;
+     for(int i=0;i< tempSize ;i++){
+         
+         
+     }
+    }
