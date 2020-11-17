36fa666e67f63ee5825aebb7f8ec4f2d8d4e4896
pranathnaik <pranathnaik12@gmail.com>
2020-11-17 20:03:55 +0530

    majority element moore's algo

--- Arrays-maths/majorityel.cpp
+++ Arrays-maths/majorityel.cpp
@@ -1,30 +1,28 @@
 #include<iostream>
-using namespace std;
-int main(){
-    int size;
-    cin>>size;
-    int a[size];
-    
-    for (int i = 0; i < size; i++)
-        cin>>a[i];
-        int ele;
-        int count=0;
-    for (int i = 0; i < size; i++)
-        {
-            if(count==0)
-            {
-                ele = a[i];
-            }
 
-            if(ele==a[i])
-            {
-                count++;
-            }
-            else
-            {
-                count--;
-            }
-            
+using namespace std;
+int main() {
+    int size;
+    cin >> size;
+    int a[size];
+
+    for (int i = 0; i < size; i++)
+        cin >> a[i];
+        
+    int ele;
+    int count = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (count == 0) {
+            ele = a[i];
         }
-        cout<<ele;
+
+        if (ele == a[i]) {
+            count++;
+        } else {
+            count--;
+        }
+
+    }
+    cout << ele;
 }
