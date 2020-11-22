1cf4f594a6c5e8a805754eaa277299c2b5a0f6e0
pranathnaik <pranathnaik12@gmail.com>
2020-11-22 21:57:21 +0530

    😴😴😴😴😴😴

---
+++ Hashing/twosum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+    int size;
+    cin>>size;
+    int a[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin>>a[i];
+    }
+    
+    
+}
---
+++ Hashing/twosum.java
@@ -0,0 +1,24 @@
+package Hashing;
+import java.util.Scanner;
+class twosum{
+    int key;
+    int val;
+    twosum(int key,int val){
+        this.key=key;
+        this.val=val;
+    }
+
+    public static void main(String[] args){
+
+        Scanner scan= new Scanner(System.in);
+        int size=scan.nextInt();
+
+        int[] arr=new int[size];
+
+        for(int i=0;i<size;i++)
+            arr[i]=scan.nextInt();
+
+        for(int i=0;i<size;i++)
+            
+    }
+}
